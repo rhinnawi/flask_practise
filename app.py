14a27cd542b6705067004eed249a78c46934e6b2
rhinnawi <rhinnawi95@gmail.com>
2020-04-13 20:07:53 -0400

    set up a mysql database, as well as functions in app.py to call on it

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
 # import Flask module
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml
 
 # Instantiate Flask object (the app)
 app = Flask(__name__)
 Bootstrap(app)
+
+# Configure db
+db = yaml.load(open('db.yaml')) # used to configure db according to data in .yaml file, named db.yaml
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app) #instantiate MySQL module
 
 '''
 App object has a decorator called route
 route used to add endpoints ('/<anything>') to application
 
 Need function to handle the decorator
-'''
-@app.route('/')
-def index():
-    sächsische_dorfer = ['Meißen', 'Freiberg', 'Moritzburg', 'Radebeul']
 
+Second paramter is a list of HTTP requests accepted by app. Defaults to GET
+'''
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO user VALUES(%s)", ['Jonas'])
+    mysql.connection.commit()
+    num_users = cur.execute("SELECT * FROM user")
+
+    sächsische_dorfer = ['Meißen', 'Freiberg', 'Moritzburg', 'Radebeul']
+    if request.method == 'POST': 
+        return request.form['password']
+        #return 'Successfully registered!'
     return render_template('index.html', dörfer=sächsische_dorfer) # rendered index.html 
     '''
     url_for can construct URLs for other routes
